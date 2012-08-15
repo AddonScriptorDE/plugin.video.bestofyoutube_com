36366b505d78e610637be8b88f4e1208dd865884
Hans Wurst <addonscriptorde@yahoo.de>
2012-08-15 22:14:33 +0200

    Version 1.0.6

--- default.py
+++ default.py
@@ -83,7 +83,7 @@ def listVideos(url):
 
 def getUrl(url):
         req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/14.0')
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:14.0) Gecko/20100101 Firefox/14.0')
         response = urllib2.urlopen(req)
         link=response.read()
         response.close()
